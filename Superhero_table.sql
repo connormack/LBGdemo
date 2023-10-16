85046292a378b628f82236da1101109cb9ea6c4a
Connor Mackintosh <67700605+connormack@users.noreply.github.com>
2023-10-16 15:02:22 +0100

    Create Superhero_table.sql

---
+++ Superhero_table.sql
@@ -0,0 +1,13 @@
+INSERT into Hero
+ (Firstname, Lastname, Alias, Ability, TeamID)
+values
+ ('Bruce', 'Wayne', 'Batman', 'Martial Arts', 1),
+    ('Clark', 'Kent', 'Superman', 'Flight', 1),
+    ('Jay', 'Garrick', 'The Flash', 'Super-Speed', 2),
+    ('Alan', 'Scott', 'Green Lantern', 'Ring Creation', 2),
+    ('Helena', 'Bertenelli', 'The Huntress', 'Crossbow Archery', 3),
+    ('Harleen', 'Quinzel', 'Harley Quinn', 'Hammer-Fighting', 3),
+    ('Floyd', 'Lawton', 'Deadshot', 'Marksmanship', 4),
+    ('Cecil', 'Adams', 'Count Vertigo', 'Induce dizziness', 4),
+    ('Damian', 'Wayne', 'Robin', 'Swordsmanship', 5),
+    ('Dick', 'Grayson', 'Nightwing', 'Acrobatics', 5);
