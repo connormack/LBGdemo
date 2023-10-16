be51e2c2e68e28a9be59cc074bb8cbe82ddf0a7d
Connor Mackintosh <67700605+connormack@users.noreply.github.com>
2023-10-16 15:01:04 +0100

    Create Team_table.sql

---
+++ Team_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO `` (`TeamID`,`TeamName`,`TeamObjective`) VALUES (1,'JLA','Protect the world');
+INSERT INTO `` (`TeamID`,`TeamName`,`TeamObjective`) VALUES (2,'JSA','Defeat the Nazis');
+INSERT INTO `` (`TeamID`,`TeamName`,`TeamObjective`) VALUES (3,'Birds of Prey','Fight crime (without men)');
+INSERT INTO `` (`TeamID`,`TeamName`,`TeamObjective`) VALUES (4,'Tax Force X','Follow Waller\'s orders or die');
+INSERT INTO `` (`TeamID`,`TeamName`,`TeamObjective`) VALUES (5,'Teen Titans','Teach young superheroes to be their best');
